df2a875cdd8f45da9448cd105befd9613bb75ad3
lucasz10 <lucas.zach10@gmail.com>
2022-11-17 20:50:29 -0700

    Set up Navigation and Project components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { Header, Footer } from "./components";
+import { Header, Footer, Navigation } from "./components";
 
-import AboutMe from "./pages/AboutMe";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
+import { AboutMe, Contact, Portfolio, Resume } from "./pages";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/aboutMe" element={<AboutMe />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
-      <Footer />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <Navigation />
+        <div className="container">
+          <Routes>
+            <Route path="/aboutMe" element={<AboutMe />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
