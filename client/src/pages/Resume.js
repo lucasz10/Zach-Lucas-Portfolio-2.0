78f3baa5b3c39f9dc5f67660a2c3a1ff7bc9d077
lucasz10 <lucas.zach10@gmail.com>
2022-11-27 21:46:51 -0700

    Added assets for resume page. App sucessfully downloads resume

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import resume from "../assets/files/Zach_Resume_2022_Updated.pdf";
+import resumePic from "../assets/images/resume_pic.PNG";
 
-const Resume = () => {};
+const Resume = () => {
+  return (
+    <div>
+      <div className="row justify-content-center">
+        <img
+          src={resumePic}
+          className="border-dark border-4 w-50"
+          alt="Zach Lucas' resume."
+        />
+      </div>
+      <div className="container text-center my-3">
+        <a href={resume} download="Zach_Lucas_Resume">
+          <Button variant="dark">Download Resume Here</Button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Resume;
