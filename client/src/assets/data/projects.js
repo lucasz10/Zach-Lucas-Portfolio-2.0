e1bb433b8f1e0e6c00604483b41cbb018f31db8e
lucasz10 <lucas.zach10@gmail.com>
2022-11-30 16:22:35 -0700

    Fixed images not loading properly in portfolio

--- client/src/assets/data/projects.js
+++ client/src/assets/data/projects.js
@@ -1,42 +1,49 @@
+import volleyballImg from "../images/volleyball-team-management_landingPage.png";
+import typeGameImg from "../images/TypeGame_landingpagepic.png";
+import projectPlanImg from "../images/projectPlanner_landingPage.png";
+import weatherImg from "../images/weatherSearchDashboard_landingPage.png";
+import passwordImg from "../images/passwordGenerator_landingPage.png";
+import pizzaImg from "../images/pizzaApp_landingPage.png";
+
 const projects = [
   {
     title: "Volleyball Team Management",
-    imgSrc: "assets/volleyball-team-management_landingPage.png",
+    imgSrc: volleyballImg,
     desc: "Full-Stack Application",
     repo: "https://github.com/lucasz10/volleyball-team-management",
     website: "https://volleyball-team-management.herokuapp.com/",
   },
   {
     title: "Type the Word or Explode!",
-    imgSrc: "assets/TypeGame_landingpagepic.png",
+    imgSrc: typeGameImg,
     desc: "Front-End Game Application",
     repo: "https://github.com/lucasz10/Type-the-Word-or-Explode",
     website: "https://lucasz10.github.io/Type-the-Word-or-Explode/",
   },
   {
     title: "Project Planner",
-    imgSrc: "assets/projectPlanner_landingPage.png",
+    imgSrc: projectPlanImg,
     desc: "Front-End Planning Application",
     repo: "https://github.com/lucasz10/Project-Day-Planner",
     website: "https://lucasz10.github.io/Project-Day-Planner/",
   },
   {
     title: "Weather Search Dashboard",
-    imgSrc: "assets/weatherSearchDashboard_landingPage.png",
+    imgSrc: weatherImg,
     desc: "Front-End Weather Dashboard",
     repo: "https://github.com/lucasz10/Weather-Search-Dashboard",
     website: "https://lucasz10.github.io/Weather-Search-Dashboard/",
   },
   {
     title: "Random Password Generator",
-    imgSrc: "assets/passwordGenerator_landingPage.png",
+    imgSrc: passwordImg,
     desc: "Front-End User Password Generator",
     repo: "https://github.com/lucasz10/Zach-Lucas-Password-Generator",
     website: "https://lucasz10.github.io/Zach-Lucas-Password-Generator/",
   },
   {
     title: "Pizza Management Application",
-    imgSrc: "assets/pizzaApp_landingPage.png",
+    imgSrc: pizzaImg,
     desc: "Full-Stack MERN Application",
     repo: "https://github.com/lucasz10/pizza-management-application",
     website: "https://pizza-management-app.herokuapp.com/login",
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Project from "../components/Project";
 
-import projects from "../assets/data/projects";
-
 const Portfolio = () => {
   return (
     <div>
-      <Project projects={projects} />
+      <Project />
     </div>
   );
 };
